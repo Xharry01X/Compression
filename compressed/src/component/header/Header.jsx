ebc492ba443ee382a6f40802be54705f14b4f9aa
harshit-506 <supriyamohapatra658@gmail.com>
2024-05-02 12:15:15 +0530

    header complete changes made time to time

--- compressed/src/component/header/Header.jsx
+++ compressed/src/component/header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
+
+import lightning from "../assets/lightning.png"
+import "./Header.scss"
+
 
 const Header = () => {
+  const [arrowDown,setArrowDown]=useState(false)
+
+  const toggleArrowDir=()=>{
+    setArrowDown(!arrowDown)
+  }
   return (
-    <div>
-      header
+    <div className='main-header'>
+      
+        <div className="header-content">
+          <div className="left">
+            <h3>𝒸ℴ𝓂𝓅𝓇ℯ𝓈𝓈𝒾𝒻𝓎
+              <img className='light' src={lightning} alt='bolt'/>
+            </h3>
+          </div>
+          <div className="center">
+          <h4 className={arrowDown ? "arrow-down" : "arrow-up"} onClick={toggleArrowDir}>
+            Convert extensions {arrowDown ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowUp />}
+          </h4>
+          </div>
+          <div className="right">
+            <button className='login'>Login</button>
+            <button className='signUP'>signUP</button>
+          </div>
+        </div>
+      
     </div>
   )
 }
